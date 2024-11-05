4c6fe1519afe994e75fb10c375faa20b8d5bfae2
Cesar Mendivil <mendivilrubioc@gmail.com>
2024-11-04 19:35:45 -0700

    Refactor e incluir la consulta de canales por id

--- src/controllers/m3uController.js
+++ src/controllers/m3uController.js
@@ -1,34 +1,17 @@
+const { response } = require('express');
+
 class M3UController {
     async parseM3U(req, res) {
         const { url } = req.body;
-
         if (!url) {
             return res.status(400).json({ error: "URL del archivo M3U es requerida." });
         }
-
         try {
             const m3uService = require('../services/m3uService');
             const parsedData = await m3uService.parseM3U(url);
+            console.log('Datos parseados:', parsedData);
 
-            const response = {
-                providerName: "Roku Developers",
-                language: "en-US",
-                lastUpdated: new Date().toISOString(),
-                listm3u8: parsedData.map(item => ({
-                    longDescription: "Learn how streaming works on the Roku platform. This video explains how content is delivered from a content delivery network (CDN) to a Roku device via a content feed",
-                    thumbnail: item.logo,
-                    releaseDate: "2020-01-15",
-                    genres: "educational",
-                    tags: "getting-started",
-                    id: "52ac61e1bfc2459aae0388731616ac9f",
-                    shortDescription: "Learn how streaming works on the Roku platform. This video explains how content is delivered from a content delivery network (CDN) to a Roku device via a content feed",
-                    title: item.title,
-                    videoType: "HLS",
-                    url: item.url,
-                    quality: "HD"
-                }))
-            };
-
+            const response = await this.buildResponse(parsedData);
             return res.json(response);
         } catch (error) {
             return res.status(500).json({ error: "Error al analizar el archivo M3U." });
@@ -62,29 +45,59 @@ class M3UController {
                 return res.status(404).json({ error: "No se encontraron resultados para el título proporcionado." });
             }
 
-            const response = {
-                providerName: "Roku Developers",
-                language: "en-US",
-                lastUpdated: new Date().toISOString(),
-                listm3u8: filteredData.map(item => ({
-                    longDescription: "Learn how streaming works on the Roku platform. This video explains how content is delivered from a content delivery network (CDN) to a Roku device via a content feed",
-                    thumbnail: item.logo,
-                    releaseDate: "2020-01-15",
-                    genres: "educational",
-                    tags: "getting-started",
-                    id: "52ac61e1bfc2459aae0388731616ac9f",
-                    shortDescription: "Learn how streaming works on the Roku platform. This video explains how content is delivered from a content delivery network (CDN) to a Roku device via a content feed",
-                    title: item.title,
-                    videoType: "HLS",
-                    url: item.url,
-                    quality: "HD"
-                }))
-            };
+            const response = await this.buildResponse(filteredData);
 
             return res.json(response);
         } catch (error) {
             return res.status(500).json({ error: "Error al analizar el archivo M3U." });
         }
+    }
+
+    async determineUrl(req, res) {
+        const { id } = req.params;
+        const urlList = [
+            { id: '1', url: 'https://support.nextream.net/mylist.m3u8' },
+            { id: '2', url: 'http://example.com/playlist2.m3u' },
+            { id: '3', url: 'http://example.com/playlist3.m3u' }
+        ];
+        
+        const item = urlList.find(item => item.id === id);
+        const m3uService = require('../services/m3uService');
+        const getData = await m3uService.parseM3U(item.url);
+        console.log('Datos parseados:', getData);
+
+
+        if (!item) {
+            return res.status(404).json({ error: "ID no encontrado." });
+        }
+
+        try {
+            const response = await this.buildResponse(getData);
+            return res.json(response);
+        } catch (error) {
+            return res.status(500).json({ error: "Error al analizar el archivo M3U." });
+        }
+    }
+
+    async buildResponse(parsedData) {
+        return {
+            providerName: "Roku Developers",
+            language: "en-US",
+            lastUpdated: new Date().toISOString(),
+            listm3u8: parsedData.map(item => ({
+                longDescription: "Learn how streaming works on the Roku platform. This video explains how content is delivered from a content delivery network (CDN) to a Roku device via a content feed",
+                thumbnail: item.logo,
+                releaseDate: "2020-01-15",
+                genres: "educational",
+                tags: "getting-started",
+                id: "52ac61e1bfc2459aae0388731616ac9f",
+                shortDescription: "Learn how streaming works on the Roku platform. This video explains how content is delivered from a content delivery network (CDN) to a Roku device via a content feed",
+                title: item.title,
+                videoType: "HLS",
+                url: item.url,
+                quality: "HD"
+            }))
+        };
     }
 }
 
