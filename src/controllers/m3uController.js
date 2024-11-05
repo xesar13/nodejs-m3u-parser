7f952f60049343f27ef1db82fe8584dc0a1e81a4
Cesar Mendivil <mendivilrubioc@gmail.com>
2024-11-04 19:51:19 -0700

    Update list m3u

--- src/controllers/m3uController.js
+++ src/controllers/m3uController.js
@@ -57,7 +57,7 @@ class M3UController {
         const { id } = req.params;
         const urlList = [
             { id: '1', url: 'https://support.nextream.net/mylist.m3u8' },
-            { id: '2', url: 'http://example.com/playlist2.m3u' },
+            { id: '2', url: 'http://plex.nextream.net/mylist.m3u' },
             { id: '3', url: 'http://example.com/playlist3.m3u' }
         ];
         
